c8b2c4e9946aa736b187a4c55919cbee91abfa06
АЛЕКСЕЙ <@192.168.1.2>
2013-12-29 13:28:52 +0300

    Добавляю классы GarageDoor, GarageDoorDownCommand и GarageDoorUpCommand

---
+++ src/smol/GarageDoorDownCommand.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smol;
+
+/**
+ *
+ * @author АЛЕКСЕЙ
+ */
+public class GarageDoorDownCommand implements Command{
+    
+    GarageDoor garageDoor;
+    
+    public GarageDoorDownCommand(GarageDoor garageDoor) {
+        this.garageDoor = garageDoor;
+    }
+    
+    @Override
+    public void execute() {
+        garageDoor.down();
+    }
+    
+    /**
+     *
+     */
+    @Override
+    public void undo() {
+        garageDoor.up();
+    }
+
+}
